4cf23f53dbff6913f5e4b89f8ff29320de1b0891
VacaSama <klc_moore@outlook.com>
2026-01-25 16:38:04 -0500

    Both the Entry class and the User Profile have been updated with properties, attributes to be added when database structuring begins.

--- Models/UserProfile.cs
+++ Models/UserProfile.cs
@@ -7,4 +7,33 @@
 /// </summary>
 public class UserProfile
 {
+    /// <summary>
+    /// Sets the unique identifier for the user made entry
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the experience level associated with the user,
+    /// and their codin skills asociated with each selected one.
+    /// </summary>
+    public int ExperienceLevel { get; set; }
+
+    /// <summary>
+    /// Asks the user what coding languages they are currently learning. 
+    /// </summary>
+    public string LearningLanguages { get; set; }
+
+    /// <summary>
+    /// Asks the user what their goal is for using this application
+    /// </summary>
+    public string IntendedUse { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether cookies are enabled for the current session.
+    /// Cookies are enabled by default to trigger the questionnaire only once, 
+    /// for new users. Returning users will not see the questionnaire again, but can 
+    /// update their response. 
+    /// </summary>
+    public bool CookiesEnabled { get; set; } = true;
+
 }
