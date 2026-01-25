46d8d153cb3443389bfa37e2a46713c44998dc40
VacaSama <klc_moore@outlook.com>
2026-01-25 16:02:18 -0500

    Two models have been created: Entry and UserProfile. The base to the other models.

---
+++ Models/Entry.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectDungbeetle.Models;
+/// <summary>
+/// The Entry class that represents a User created entry. 
+/// Entries are notes or errors that the user wants to keep track of, 
+/// it also acts as a reflective log that the user can use to improve their 
+/// skills in computer programming and debugging. 
+/// </summary>
+public class Entry
+{
+}
---
+++ Models/UserProfile.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectDungbeetle.Models;
+
+/// <summary>
+/// The UserProfile class that represents a user's profile information.
+/// It keeps track of what coding languages the user is learning, their skill level, 
+/// the intended use of the application, cookie detection, and other relevant details.
+/// </summary>
+public class UserProfile
+{
+}
