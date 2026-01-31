54097c1887a7bca7d066f058adf40c58689a9c06
VacaSama <klc_moore@outlook.com>
2026-01-31 15:54:37 -0500

    The models are being used as reference and have been declared in the `DungbeetleDbContext.cs`, again please note. The Application name is "PROJECT DUNGBEETLE", however the DB is named just "DungbeetleDbContext" to shorten the already lengthy text!!

--- Data/DungbeetleDbContext.cs
+++ Data/DungbeetleDbContext.cs
@@ -1,10 +1,42 @@
 ﻿namespace ProjectDungbeetle.Data;
 using Microsoft.EntityFrameworkCore;
+using ProjectDungbeetle.Models;
+using Microsoft.Identity.Client;
 
 public class DungbeetleDbContext : DbContext
 {
     public DungbeetleDbContext(DbContextOptions<DungbeetleDbContext> options) : base(options)
     {
-
+        // nothing goes here :) 
     }
+
+    // declare DBSets/Models/ aka tables here 
+
+    /// <summary>
+    /// Retrieves or sets the collection of Entry objects in the database.
+    /// </summary>
+    public DbSet<Entry> Entries { get; set; }
+
+    /// <summary>
+    /// Retrieves or sets the collection of Hints objects in the database.
+    /// </summary>
+    public DbSet<Hints> Hints { get; set; }
+
+    /// <summary>
+    /// Retrieves or sets the collection of Questionnaire(questions) objects in the database.
+    /// </summary>
+    public DbSet<Questionnaire> Questionnaires { get; set; }
+
+    /// <summary>
+    /// Retrieves or sets the collection of QuestionnaireResponse objects in the database.
+    /// Based per user.
+    /// </summary>
+    public DbSet<QuestionnaireResponse> QuestionnaireResponses { get; set; }
+
+    /// <summary>
+    /// Retrieves or sets the collection of UserProfile objects in the database.
+    /// Based per user.
+    /// </summary>
+    public DbSet<UserProfile> UserProfiles { get; set; }
+
 }
