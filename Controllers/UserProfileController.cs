b9f5bf48ce8b300bb1c5e0f8620b0c62ec7f6b36
VacaSama <klc_moore@outlook.com>
2026-03-02 15:24:39 -0500

    *** NEW ITEM(S) ADDED - UserProfileController and the Index for the UserController. There will be a redirect on the nav bar for the user profile, this is where the user will be able to view and modify their user response answers -- and provide other basic information about themselves without complete login.

---
+++ Controllers/UserProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectDungbeetle.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
