8a0b45657923575586ad56da582522713c6561c6
VacaSama <klc_moore@outlook.com>
2026-03-02 16:52:58 -0500

    The Home Link/button has been changed to "Dashboard" I wanted to distinguish a little bit better User Profile vs Home.
    Setting up the questionnaire logic in the UserProfileController, I plan to call the method from the User Profile using JS and The Home Controller to give life to the questionnaire. Later on, I will complete the logic for my "time machine" function it's intended purpose is for testing only, to make sure the questionnaire works means I would have to complete it myself. But upon completion I won't be able to revisit that process, so I am preemptively helping myself out.

--- Controllers/UserProfileController.cs
+++ Controllers/UserProfileController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using ProjectDungbeetle.Models;
+using ProjectDungbeetle.ViewModels;
+using ProjectDungbeetle.Data;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ProjectDungbeetle.Controllers
 {
     public class UserProfileController : Controller
     {
+        /// <summary>
+        /// A readonly field that holds the database context for the application. 
+        /// </summary>
+        private readonly DungbeetleDbContext _context;
+
+        /// <summary>
+        /// This is a constructor fo the UserProfileController class to retieve the database context.
+        /// </summary>
+        /// <param name="context"></param>
+        public UserProfileController(DungbeetleDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        // Retrieves the questions from the database for the user to answer
+        // or view
+        [HttpGet]
+        public IActionResult getQuestions()
+        {
+            // get the questions from the DB
+            var questions = _context.Questionnaires.
+                OrderBy(q => q.QuestionText).
+                ToList();
+
+            return Json(questions);
         }
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+async function checkAndShowModal() { 
+    // check Local Storage to see if the user has been here before 
+    const hasCompletedQuest = localStorage.getItem("user_completed_quest");
+
+
+    // if the user has NOT completed the questionnaire, show the modal 
+    if (!hasCompletedQuest) { 
+        // try to grab the modal elements
+        // the questions
+        // the user response
+
+        // display the questions onto the modal
+
+        // open the modal 
+    }
+}
+
+// create a function that restarts the questionnaire prompt, and clears it from the local storage
+function resetQuest() {
+} 
