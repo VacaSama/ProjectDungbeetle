1c041ec9fff8c6465d4f5d7f3c9a25ce8c6f6577
VacaSama <klc_moore@outlook.com>
2026-03-07 15:45:04 -0500

    Adding a migration to solidify new data types, enums.

---
+++ Data/Migrations/20260307203756_UserProfile-and-Enums.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectDungbeetle.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserProfileandEnums : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "LearningLanguages",
+                table: "UserProfiles",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "IntendedUse",
+                table: "UserProfiles",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ExperienceLevel",
+                table: "UserProfiles",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "UserProfiles",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Occupation",
+                table: "UserProfiles",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "UserProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "Occupation",
+                table: "UserProfiles");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LearningLanguages",
+                table: "UserProfiles",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IntendedUse",
+                table: "UserProfiles",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ExperienceLevel",
+                table: "UserProfiles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
